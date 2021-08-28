6b9f98441a4c7a7643a2eb0eb7bd8b092dc1d72c
Colin Watts <colinjoelwatts@gmail.com>
2021-08-28 09:19:36 -0400

    Fixing up timezone stuff and adding scheduler utilities

--- Scheduler/TaskScheduler.py
+++ Scheduler/TaskScheduler.py
@@ -1,13 +1,25 @@
 import sys
-import datetime
+import datetime as dt
 import pytz
 from datetime import datetime
 from datetime import timezone
 from datetime import timedelta
-import time
+import time as t
 from Scheduler.Task import Task
 from Scheduler.SendStatusToDiscordTask import SendStatusToDiscordTask 
 from Scheduler.TweetRandomImageFromSourceTask import TweetRandomImageFromSourceTask
+
+# note, incoming timezone is assumed to be "America/New_York"
+def GetNextOccurenceOfESTTime(time):
+    estTimeZone = pytz.timezone("America/New_York")
+    testTime = datetime.now()
+    testTime = testTime.replace(hour=time.hour, minute=time.minute, second=time.second, microsecond=0)
+    testTime = estTimeZone.localize(testTime)
+    if testTime > datetime.now(timezone.utc):
+        return testTime
+    else:
+        day = timedelta(days=1)
+        return testTime + day
 
 class TaskScheduler:
     def __init__(self, config, twitterManager, imgManager, discordManager):
@@ -16,7 +28,7 @@ class TaskScheduler:
         self.imageManager = imgManager
         self.discordManager = discordManager
         self.TaskList = []
-        self.LoadTaskSchedule()
+        self.LoadDailySchedule()
 
     def Run(self):
         while True:
@@ -28,30 +40,51 @@ class TaskScheduler:
                     task.IncrementTime() # we make use of the increment function to avoid error spam
                     
             # Pause the thread until we try again
-            time.sleep(self.config["TaskTryRunFreqSec"])
+            t.sleep(self.config["TaskTryRunFreqSec"])
     
+    def LoadDailySchedule(self):
+        dayinterval = timedelta(days=1)
+
+        sendStatusTime = GetNextOccurenceOfESTTime(dt.time(hour=10, minute=0))
+        sendStatusTask = SendStatusToDiscordTask(self.imageManager, self.discordManager, self.twitterManager, sendStatusTime, dayinterval)
+        self.TaskList.append(sendStatusTask)
+
+        NineAM = GetNextOccurenceOfESTTime(dt.time(hour=9, minute=0))
+        tweetImage9AMTask = TweetRandomImageFromSourceTask(self.imageManager, self.discordManager, self.twitterManager, NineAM, dayinterval)
+        self.TaskList.append(tweetImage9AMTask)
+
+        Noon = GetNextOccurenceOfESTTime(dt.time(hour=12, minute=0))
+        tweetImageNoonTask = TweetRandomImageFromSourceTask(self.imageManager, self.discordManager, self.twitterManager, Noon, dayinterval)
+        self.TaskList.append(tweetImageNoonTask)
+
+        ThreePM = GetNextOccurenceOfESTTime(dt.time(hour=15, minute=0))
+        tweetImage3PMTask = TweetRandomImageFromSourceTask(self.imageManager, self.discordManager, self.twitterManager, ThreePM, dayinterval)
+        self.TaskList.append(tweetImage3PMTask)
 
     def LoadTaskSchedule(self):
         # This is where we load in a schedule
         # Currently, schedule is hardcoded
-
+        estTimeZone = pytz.timezone("America/New_York")
+        dayinterval = timedelta(days=1)
         # Example Default Task
         #self.TaskList.append(Task(datetime.datetime.now(timezone.utc) + datetime.timedelta(seconds = 10), datetime.timedelta(seconds = 10), maxRuns=60))
         
         # set up a status reminder task
-        sendStatusStartTime = self.ESTtoUTC(datetime(2021, 8, 24, hour=8, minute=30))
-        sendStatusInterval = timedelta(minutes=180)
-        sendStatusTask = SendStatusToDiscordTask(self.imageManager, self.discordManager, self.twitterManager, sendStatusStartTime, sendStatusInterval)
+        sendStatusStartTime = estTimeZone.localize(datetime(2021, 9, 1, hour=10))
+        sendStatusTask = SendStatusToDiscordTask(self.imageManager, self.discordManager, self.twitterManager, sendStatusStartTime, dayinterval)
         self.TaskList.append(sendStatusTask)
 
         # set up a task to tweet images
-        tweetImageStartTime = self.ESTtoUTC(datetime(2021, 8, 24, hour=8, minute=30))
-        tweetImageInterval = timedelta(minutes=30)
-        tweetImageTask = TweetRandomImageFromSourceTask(self.imageManager, self.discordManager, self.twitterManager, tweetImageStartTime, tweetImageInterval)
-        self.TaskList.append(tweetImageTask)
+        NineAM = estTimeZone.localize(datetime(2021, 9, 1, hour=9))
+        tweetImage9AMTask = TweetRandomImageFromSourceTask(self.imageManager, self.discordManager, self.twitterManager, NineAM, dayinterval)
+        self.TaskList.append(tweetImage9AMTask)
 
-    def ESTtoUTC(self, estTime):
-        utcTime = estTime + timedelta(hours = -self.config["EasterStandardTimeOffset"])
-        return utcTime.replace(tzinfo=pytz.UTC)
-    def UTCtoEST(self, utcTime):
-        return utcTime + timedelta(hours = self.config["EasterStandardTimeOffset"])
+        Noon = estTimeZone.localize(datetime(2021, 9, 1, hour=12))
+        tweetImageNoonTask = TweetRandomImageFromSourceTask(self.imageManager, self.discordManager, self.twitterManager, Noon, dayinterval)
+        self.TaskList.append(tweetImageNoonTask)
+
+        ThreePM = estTimeZone.localize(datetime(2021, 9, 1, hour=15))
+        tweetImage3PMTask = TweetRandomImageFromSourceTask(self.imageManager, self.discordManager, self.twitterManager, ThreePM, dayinterval)
+        self.TaskList.append(tweetImage3PMTask)
+
+
